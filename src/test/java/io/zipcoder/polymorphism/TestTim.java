332d506646585c14f42ce8f89daf0496d9002826
Timothy <timothy@Zipcoders-MacBook-Pro-3.local>
2023-03-15 19:18:04 -0400

    added changes

--- src/test/java/io/zipcoder/polymorphism/TestTim.java
+++ src/test/java/io/zipcoder/polymorphism/TestTim.java
@@ -1,4 +1,42 @@
 package io.zipcoder.polymorphism;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestTim {
+//    Create a program that asks the user how many pets they have.
+//    Once you know how many pets they have, ask them what kind of pet each one is,
+//    along with each pet's name. For now your program should just hold onto the user
+//    input and print out the list at the end; we'll modify this in part 3.
+
+
+
+    @Test
+    public void testCreateRedPanda(){
+        //given
+        Pet panda = new redPanda();
+        String expectedGender = "Male";
+        String expectedName = "James";
+
+        // When
+        Pet panda = new redPanda("James", "Male");
+
+        // Then
+        String actualGender = panda.getGender();
+        String actualName = panda.getName();
+
+        Assert.assertEquals(expectedGender, actualGender);
+        Assert.assertEquals(expectedName, actualName);
+
+    }
+
+    @Test
+    public void testSpeak(){
+        Pet panda = new redPanda("James", "Male");
+        String expectedSpeak = "Ah!";
+
+        panda.speak();
+        Assert.assertEquals("Ah!", expectedSpeak);
+
+    }
 }
