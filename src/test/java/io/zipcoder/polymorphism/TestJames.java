9301fca6957dabed8587aea3fa148ed3a4aa96dd
James <james@Zipcoders-MacBook-Pro-101.local>
2023-03-15 19:20:15 -0400

    started dog test

--- src/test/java/io/zipcoder/polymorphism/TestJames.java
+++ src/test/java/io/zipcoder/polymorphism/TestJames.java
@@ -1,9 +1,58 @@
 package io.zipcoder.polymorphism;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestJames {
 @Test
-    public void testMe(){}
+    public void testCreateDog(){
+    // Given
+    String expectedName = "PimTark";
+    Integer expectedAge = 2;
+
+    // When
+    Pet dog = new Pet();
+    String actualName = dog.getName();
+    Integer actualAge = dog.getAge();
+    // Then
+    Assert.assertEquals(expectedName, actualName);
+    Assert.assertEquals(expectedAge, actualAge);
+    }
+    public void testSetName(){
+    // Given
+    String expectedName = "PimTark";
+    // When
+    Pet dog =  new Pet();
+    String actualName = dog.setName();
+
+    // Then
+    Assert.assertEquals(expectedName, actualName);
+    }
+    public void testSpeak(){
+        // Given
+        String expectedSpeak = "Woof!";
+        Pet dog = new Pet();
+
+        // When
+        dog.speak();
+
+        // Then
+        Assert.assertEquals(expectedSpeak, "Woof!");
+    }
+    public void testGetName(){
+        // Given
+
+
+        // When
+
+
+        // Then
+
+
+
+    }
+
+
 }
+
